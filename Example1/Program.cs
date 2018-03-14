335fb125ba19229edbe3e06049b24c804038f09b
Ian Morenot <imorenodev@gmail.com>
2018-03-13 22:21:52 -0400

    Added example Person class with getter setter

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -7,7 +7,21 @@ namespace Example1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Again!");
+
+            Person p1 = new Person();
+            try
+            {
+                p1.Age = int.Parse(Console.ReadLine());
+            } catch (FormatException e) {
+                Console.WriteLine("Error: " + e);
+            }
+
+            Console.WriteLine("Hello Again! You are " + p1.Age);
         }
+    }
+
+    class Person
+    {
+        public int Age { get; set; } = 5;
     }
 }
